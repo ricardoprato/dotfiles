487d3f91d7d5d3b9877948d12dc465165f205fc0
ricardoprato <ricardoprato36@gmail.com>
2024-12-22 15:41:48 -0300

    Better layout

--- configs/dot-config/fish/config.fish
+++ configs/dot-config/fish/config.fish
@@ -1,21 +1,22 @@
-# fisher
-set -g fisher_path $HOME/.local/share/fish/fisher
-set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
-set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
-for file in $fisher_path/conf.d/*.fish
-  builtin source $file 2> /dev/null
+set -g fish_greeting
+
+if status is-interactive
+    starship init fish | source
 end
 
-if status is-interactive && ! functions --query fisher
-  curl --silent --location https://git.io/fisher | source && fisher install jorgebucaran/fisher
-end
+# List Directory
+alias l='eza -lh  --icons=auto' # long list
+alias ls='eza -1   --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+alias lt='eza --icons=auto --tree' # list folder as tree
 
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
 
-if test -z "$NVIM"
-  pokemon-colorscripts --no-title -r
-end
-
-# Initialize Starship and Zoxide
-zoxide init fish | source
-starship init fish | source
-
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
